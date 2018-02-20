86d1e8dff2e496502b9508c80b7ee4129fbdc3bd
Kevin Marete <kevomarete@gmail.com>
2018-02-21 01:17:34 +0300

    #Added: Regimen List View

--- config/views.sql
+++ config/views.sql
@@ -6,3 +6,13 @@ CREATE OR REPLACE VIEW vw_drug_list AS
 	INNER JOIN tbl_generic g ON g.id = d.generic_id
 	INNER JOIN tbl_formulation f ON f.id = d.formulation_id
 	ORDER BY id
+
+/*Regimen List*/
+CREATE OR REPLACE VIEW vw_regimen_list AS
+	SELECT
+		r.id, CONCAT_WS(' | ', r.code, r.name) name, r.description, c.name category, s.name service, l.name line
+	FROM tbl_regimen r
+	INNER JOIN tbl_category c ON c.id = r.category_id
+	INNER JOIN tbl_service s ON s.id = r.service_id
+	INNER JOIN tbl_line l ON l.id = r.line_id
+	ORDER BY id
